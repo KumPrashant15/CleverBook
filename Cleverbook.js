786710e6ce470c5f5c73ac55a5ff8820dd493024
KumPrashant15 <prashant.k0515@gmail.com>
2024-06-04 23:43:33 +0530

    eight

--- Cleverbook.js
+++ Cleverbook.js
@@ -1,25 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById("mobile-menu");
-  const navLinks = document.querySelector(".nav-links");
-
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
-  });
-
-  const tabLinks = document.querySelectorAll(".tab-link");
-  const tabContents = document.querySelectorAll(".tab-content");
-
-  tabLinks.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      event.preventDefault();
-      tabLinks.forEach((link) => link.classList.remove("active"));
-      tabContents.forEach((content) => content.classList.remove("active"));
-
-      event.currentTarget.classList.add("active");
-      const tabContent = document.querySelector(
-        event.currentTarget.getAttribute("href")
-      );
-      tabContent.classList.add("active");
+document.addEventListener("DOMContentLoaded"),
+  () => {
+    const menuToggle = document.getElementById("mobile-menu");
+    const navLinks = document.querySelector(".nav-links");
+    // $(document).ready(function () {
+    //   $(".counter").counterUp({
+    //     delay: 10,
+    //     time: 1200,
+    //   });
+    // });
+    
+    menuToggle.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
     });
-  });
-});
+
+    const tabLinks = document.querySelectorAll(".tab-link");
+    const tabContents = document.querySelectorAll(".tab-content");
+
+    tabLinks.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        tabLinks.forEach((link) => link.classList.remove("active"));
+        tabContents.forEach((content) => content.classList.remove("active"));
+
+        event.currentTarget.classList.add("active");
+        const tabContent = document.querySelector(
+          event.currentTarget.getAttribute("href")
+        );
+        tabContent.classList.add("active");
+      });
+    });
+  };
+
+  

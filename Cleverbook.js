a93ebb8b57403eb9c640d027f815fcd66cb91b7e
KumPrashant15 <prashant.k0515@gmail.com>
2024-06-04 15:03:26 +0530

    first commit

---
+++ Cleverbook.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("mobile-menu");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
+  const tabLinks = document.querySelectorAll(".tab-link");
+  const tabContents = document.querySelectorAll(".tab-content");
+
+  tabLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      tabLinks.forEach((link) => link.classList.remove("active"));
+      tabContents.forEach((content) => content.classList.remove("active"));
+
+      event.currentTarget.classList.add("active");
+      const tabContent = document.querySelector(
+        event.currentTarget.getAttribute("href")
+      );
+      tabContent.classList.add("active");
+    });
+  });
+});

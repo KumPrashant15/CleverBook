963807523996c8d3343894243e0ede93ea6658b7
KumPrashant15 <prashant.k0515@gmail.com>
2024-06-06 00:06:46 +0530

    fifteen

---
+++ Cleverbook.js
@@ -0,0 +1,13 @@
+// document.getElementsByClassName("menu-toggle").addEventListener("click", () => {
+//   let menu = document.getElementsByClassName("menu");
+//   menu.classList.toggle("menu-visible");
+// });
+
+function handleClick() {
+  let menu = document.getElementById("hidden");
+  menu.classList.toggle("menu-visible");
+  // menu.style.display = "block";
+  // console.log(menu)
+}
+
+// console.log(document.getElementById("hidden"));

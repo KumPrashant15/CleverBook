cd1ceb2771eea87fea4b4acf99c870e5f79368ab
KumPrashant15 <prashant.k0515@gmail.com>
2024-06-06 15:20:31 +0530

    sixteen

--- Cleverbook.js
+++ Cleverbook.js
@@ -3,6 +3,23 @@
 //   menu.classList.toggle("menu-visible");
 // });
 
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
 function handleClick() {
   let menu = document.getElementById("hidden");
   menu.classList.toggle("menu-visible");

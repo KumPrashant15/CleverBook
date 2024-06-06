e36b949b334a795d88571df153bf0bc311b75f76
KumPrashant15 <prashant.k0515@gmail.com>
2024-06-06 17:57:03 +0530

    Twenty

--- Cleverbook.js
+++ Cleverbook.js
@@ -20,6 +20,42 @@ productContainers.forEach((item, i) => {
     })
 })
 
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const counters = document.querySelectorAll('.counter');
+  const speed = 200; // Speed of counting (higher is slower)
+
+  const updateCount = (counter) => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+
+      const increment = target / speed;
+
+      if (count < target) {
+          counter.innerText = Math.ceil(count + increment);
+          setTimeout(() => updateCount(counter), 10); // Adjust delay for smoother animation
+      } else {
+          counter.innerText = target;
+      }
+  };
+
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              updateCount(entry.target);
+          } else {
+              entry.target.innerText = '0'; // Reset the counter when it leaves the viewport
+          }
+      });
+  }, {
+      threshold: 0.1 // Adjust based on when you want to start the counting
+  });
+
+  counters.forEach(counter => {
+      observer.observe(counter);
+  });
+});
+
 function handleClick() {
   let menu = document.getElementById("hidden");
   menu.classList.toggle("menu-visible");
